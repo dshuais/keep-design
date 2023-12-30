20d760fa8032a57d64ddd393d90a170856e1d5bb
dushuais <1137896420@qq.com>
2023-12-30 17:58:26 +0800

    fix：组件测试demo配置

---
+++ demo/App.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Author: dushuai
+ * @Date: 2023-12-30 17:53:17
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-12-30 17:56:30
+ * @Description: App
+-->
+<template>
+  <div>启动测试</div>
+</template>
