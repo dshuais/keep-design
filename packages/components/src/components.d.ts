34697c08b260f981fc6786f4fe93b75e43227541
dshuais <1137896420@qq.com>
2024-01-02 17:05:46 +0800

    feat：配置打包模式

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -1,10 +1,3 @@
-/*
- * @Author: dushuai
- * @Date: 2023-12-30 19:29:09
- * @LastEditors: dushuai
- * @LastEditTime: 2023-12-30 19:52:19
- * @Description: 描述
- */
 import * as components from "./index";
 
 declare module "@vue/runtime-core" {
@@ -12,6 +5,6 @@ declare module "@vue/runtime-core" {
     KButton: typeof components.Button;
   }
 }
-export {};
+export { };
 
 console.log(components);
--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -2,11 +2,12 @@
  * @Author: dushuai
  * @Date: 2024-01-02 16:11:16
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-02 16:21:08
+ * @LastEditTime: 2024-01-02 16:57:07
  * @description: viteconfig
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
@@ -14,20 +15,38 @@ export default defineConfig({
     minify: false, // 是否压缩
     rollupOptions: {
       external: ['vue'], // 忽略打包vue文件
-      // input: ['index.ts'],
-      output: {
-        globals: {
-          vue: 'Vue'
+      input: ['index.ts'],
+      output: [
+        {
+          format: 'es', // 打包格式
+          entryFileNames: '[name].mjs', // 打包后的文件名
+          preserveModules: true, // 让打包目录和我们目录对应
+          exports: 'named',
+          dir: '../keepDesign/es' // 打包后的文件目录
         },
-        dir: 'dist'
-      }
+        {
+          format: 'cjs', // 打包格式
+          entryFileNames: '[name].js', // 打包后的文件名
+          preserveModules: true, // 让打包目录和我们目录对应
+          exports: 'named',
+          dir: '../keepDesign/lib' // 打包后的文件目录
+        }
+      ]
     },
     lib: {
       entry: './index.ts', // 打包入口文件
-      name: 'keepDesign',
-      fileName: 'keepDesign',
-      formats: ['es', 'umd', 'cjs']
+      // name: 'keepDesign',
+      // fileName: 'keepDesign',
+      // formats: ['es', 'umd', 'cjs']
     }
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      entryRoot: './src',
+      outputDir: ['../keepDesign/es/src', '../keepDesign/lib/src'],
+      // 指定使用的tsconfig.json为我们整个项目根目录下,如果不配置,你也可以在components下新建tsconfig.json
+      tsConfigFilePath: '../../tsconfig.json',
+    })
+  ],
 })
