e5849abecf473aa45d410aafa1b96b536433f374
dshuais <1137896420@qq.com>
2024-01-08 11:35:36 +0800

    feat：优化组件库路径

--- packages/components/script/publish/index.ts
+++ packages/components/script/publish/index.ts
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-01-03 15:18:58
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-03 18:35:04
+ * @LastEditTime: 2024-01-08 11:35:11
  * @description: 发布npm脚本
  */
 import run from '../utils/run';
@@ -10,9 +10,9 @@ import { pkgPath } from '../utils/paths';
 import { series } from 'gulp';
 
 export const publishComponent = async () => {
-  console.log(`${pkgPath}\\keepDesign`);
+  // console.log(`${pkgPath}\\KEEP_DESIGN`);
 
-  run('release-it', `${pkgPath}\\keepDesign`);
+  run('release-it', `${pkgPath}\\KEEP_DESIGN`);
 };
 
 export default series(async () => publishComponent());
