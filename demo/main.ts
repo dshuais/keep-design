dadfa5e695ec375a2a27281df167baf184594f99
dshuais <1137896420@qq.com>
2024-01-09 16:06:28 +0800

    feat: test demo build

--- demo/main.ts
+++ demo/main.ts
@@ -2,14 +2,16 @@
  * @Author: dushuai
  * @Date: 2024-01-02 15:44:32
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-08 15:15:42
+ * @LastEditTime: 2024-01-09 16:04:13
  * @description: 心平气和
  */
 import { createApp } from 'vue';
 import App from './App.vue';
-import keepDesign from '@keep-design/components';
+// import keepDesign from '@keep-design/components';
+import { KButton } from '@keep-design/components'
 
 const app = createApp(App);
 
-app.use(keepDesign);
+// app.use(keepDesign);
+app.use(KButton);
 app.mount('#app');
